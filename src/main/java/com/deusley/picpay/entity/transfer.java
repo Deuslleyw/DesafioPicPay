4eb3b2a5a4e6b9cecb752c9adf33e12e19e00ec0
Deuslleyw <Deuslleyw@hotmail.com>
2025-05-14 16:01:57 -0300

    implsClassss

---
+++ src/main/java/com/deusley/picpay/entity/transfer.java
@@ -0,0 +1,33 @@
+package com.deusley.picpay.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_transfer")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class transfer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_sender_id")
+    private Wallet sender;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_receiver_id")
+    private Wallet receiver;
+
+    private BigDecimal balance;
+
+
+}
